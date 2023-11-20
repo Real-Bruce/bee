ef397b180509ac9fafbafbc1353c43ec28d68046
Bruce <36781800+real-bruce@users.noreply.github.com>
2023-11-20 11:07:57 +0800

    init

--- bee-common/src/main/java/com/bee/common/entity/baseEntity.java
+++ bee-common/src/main/java/com/bee/common/entity/baseEntity.java
@@ -1,8 +1,12 @@
 package com.bee.common.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author Bruce
@@ -14,4 +18,21 @@ public class baseEntity implements Serializable {
 
     private static final long serialVersionUID = 5634536428309478754L;
 
+    /**
+     * Id
+     */
+    @TableId
+    private Long id;
+
+    /**
+     * 创建人
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Long creator;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createDate;
 }
