a480b3fd06732dbdaadf9b36e0ae7c1eb5bfb848
Bruce <36781800+real-bruce@users.noreply.github.com>
2023-12-06 09:59:56 +0800

    feat: add validator and tree node utils

---
+++ bee-common/src/main/java/com/bee/common/util/validator/group/DefaultGroup.java
@@ -0,0 +1,9 @@
+package com.bee.common.util.validator.group;
+
+/**
+ * @author Bruce
+ * @create 2023/12/06
+ * @description 默认Group
+ */
+public interface DefaultGroup {
+}
---
+++ bee-common/src/main/java/com/bee/common/util/validator/group/UpdateGroup.java
@@ -0,0 +1,9 @@
+package com.bee.common.util.validator.group;
+
+/**
+ * @author Bruce
+ * @create 2023/12/06
+ * @description 修改Group
+ */
+public interface UpdateGroup {
+}
