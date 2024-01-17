cb57171499c63e44ac8748e9e055837f10e89119
Bruce <36781800+real-bruce@users.noreply.github.com>
2024-01-17 16:25:00 +0800

    feat: add global configuration

---
+++ bee-ui/vite.config.ts
@@ -0,0 +1,62 @@
+import vue from "@vitejs/plugin-vue";
+import {resolve} from "path"
+import {defineConfig, loadEnv, UserConfig, UserConfigExport} from "vite";
+import html from "vite-plugin-html";
+import tsconfigPaths from "vite-tsconfig-paths";
+import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
+
+export default (config: UserConfig): UserConfigExport => {
+  const mode = config.mode as string;
+  return defineConfig({
+    base: "./",
+    plugins: [
+      vue(),
+      html({
+        inject: {
+          injectData: {
+            apiURL: loadEnv(mode, process.cwd()).VITE_APP_API,
+            title: ""
+          }
+        },
+        minify: true
+      }),
+      tsconfigPaths(),
+      createSvgIconsPlugin({
+        iconDirs: [resolve(__dirname), "src/assets/icon/svg"],
+        symbolId: "icon-[dir]-[name]"
+      })
+    ],
+
+    build: {
+      chunkSizeWarningLimit: 1024,
+      commonjsOptions: {
+        include: /node_modules|lib/
+      },
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            quill: ["quill"],
+            lodash: ["lodash"],
+            vlib: ["vue", "vue-router", "element-plus"]
+          }
+        }
+      }
+    },
+
+    resolve: {
+      alias: {
+        // 别名
+        "@": resolve(__dirname, "./src")
+      }
+    },
+
+    server: {
+      // 打开浏览器
+      open: true,
+      host: "0.0.0.0",
+      port: 9001,
+      https: false,
+      hmr: {overlay: false}
+    }
+  })
+}
