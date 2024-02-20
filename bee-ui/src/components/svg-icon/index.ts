867820822a36ed0ebb0655d4f20008306ddaee8c
Bruce <36781800+real-bruce@users.noreply.github.com>
2024-02-20 11:01:13 +0800

    feat: add svg component

---
+++ bee-ui/src/components/svg-icon/index.ts
@@ -0,0 +1,4 @@
+import { withInstall } from "@/utils/common";
+import { SvgIcon } from "./index.vue";
+
+export default withInstall(SvgIcon)
---
+++ bee-ui/src/components/svg-icon/index.vue
@@ -0,0 +1,53 @@
+<script lang="ts">
+import { computed,defineComponent} from 'vue'
+
+/**
+ * 自定义svg图标，可将svg图标下载后存放在/src/assets/icon/svg目录下
+ * 使用方式: <svg-icon name="earth" color="red"></svg-icon>
+ */
+export default defineComponent({
+  name: "SvgIcon",
+  props: {
+    prefix: {
+      type: String,
+      default: "icon"
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      default: ""
+    },
+    className: {
+      type: String,
+      default: ""
+    },
+    style: {
+      type: String,
+      default: ""
+    },
+    width: String,
+    height: String
+  },
+  setup(props) {
+    const symbolId = computed(() => `#${props.prefix}-${props.name.replace("icon", "")}`);
+    return { symbolId }
+  }
+})
+
+export class SvgIcon {
+}
+</script>
+
+<template>
+  <svg aria-hidden="true" :class="`iconfont ${className}`" :style="`width:${width};height:${height};color:${color};${style}`">
+    <use :xlink:href="symbolId"/>
+  </svg>
+
+</template>
+
+<style scoped>
+
+</style>
